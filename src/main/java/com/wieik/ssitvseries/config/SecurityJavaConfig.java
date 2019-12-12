bac4b9f142d484af6225fd132d30dd69488644d4
MatNatanek <41325433+MatNatanek@users.noreply.github.com>
2019-12-12 19:01:13 +0100

    Update SecurityJavaConfig.java

--- src/main/java/com/wieik/ssitvseries/config/SecurityJavaConfig.java
+++ src/main/java/com/wieik/ssitvseries/config/SecurityJavaConfig.java
@@ -47,7 +47,7 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("*").permitAll().anyRequest().authenticated()
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated()
         .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class).addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
