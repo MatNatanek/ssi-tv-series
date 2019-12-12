9f65a82faff2ee8375af544f1f49226db425a0d2
MatNatanek <41325433+MatNatanek@users.noreply.github.com>
2019-12-12 18:55:19 +0100

    Update SecurityJavaConfig.java

--- src/main/java/com/wieik/ssitvseries/config/SecurityJavaConfig.java
+++ src/main/java/com/wieik/ssitvseries/config/SecurityJavaConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,7 +33,6 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailService);
     }
-    
 
     @Override
     @Bean
@@ -47,11 +45,11 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated()
-        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class).addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated()
+//        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class).addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+//    }
 
 }
