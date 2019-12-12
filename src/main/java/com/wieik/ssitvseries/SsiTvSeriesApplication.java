7a61588cfaee2e5bb48db5b91d8ab3f793cfadaf
MatNatanek <41325433+MatNatanek@users.noreply.github.com>
2019-12-12 10:11:06 +0100

    Update SsiTvSeriesApplication.java

--- src/main/java/com/wieik/ssitvseries/SsiTvSeriesApplication.java
+++ src/main/java/com/wieik/ssitvseries/SsiTvSeriesApplication.java
@@ -5,16 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Collections;
 
 
 @SpringBootApplication
@@ -29,17 +20,5 @@ public class SsiTvSeriesApplication extends SpringBootServletInitializer {
 		return super.configure(builder);
 	}
 
-	@Bean
-	public FilterRegistrationBean simpleCorsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
-		config.setAllowedMethods(Collections.singletonList("*"));
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter((CorsConfigurationSource) source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
+
 }
