b9e1513b6abfccd4a0e9bd3745ad0aed52d86d34
Mateusz Natanek <mateusznatanek999@gmail.com>
2020-01-09 13:20:20 +0100

    New branch

--- src/main/java/com/wieik/ssitvseries/util/JwtRequestFilter.java
+++ src/main/java/com/wieik/ssitvseries/util/JwtRequestFilter.java
@@ -15,7 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+//comment
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
